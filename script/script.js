f1f29a5b7414a6fcf6dbd963e61f808480914107
josh rogerson <jrogerson3@my.bcit.ca>
2022-11-22 11:06:17 -0800

    event target adding button and text, changing colours

--- script/script.js
+++ script/script.js
@@ -1,46 +1,66 @@
 const clickMebutton = document.querySelector(".click_me");
 
-
-function clickReaction () {
-
-    alert("You Clicked!");
-
-    
-
+function clickReaction() {
+  alert("You Clicked!");
 }
 
-clickMebutton.addEventListener("Click", clickReaction );
+clickMebutton.addEventListener("click", clickReaction);
 
 
-
-function changeBGColour ()
-
-{
-        // document.body.style.backgroundColor = "pink";
-        document.body.classList.add("pink");
-
-    
-
+function changeBGColour() {
+  document.body.style.backgroundColor = "pink";
+  // document.body.classList.add("pink");
 }
 // clickMebutton.removeEventListener("Click", clickReaction);
 
-clickMebutton.addEventListener("Click",changeBGColour)
+clickMebutton.addEventListener("click", changeBGColour);
 
-function changeText()
+function changeText() {
+  clickMebutton.textContent = "Clicked";
+
+  if (clickMebutton.textContent === "Click Me!") {
+    clickMebutton.textContent = "Clicked";
+  }
+
+  if (clickMebutton.textContent === "Clicked") {
+    clickMebutton, (textContent = "Click Me!");
+  }
+}
+
+clickMebutton.addEventListener("click", changeText);
+
+function createAddButton()
+{
+    const purpleButton = document.createElement("button");
+    purpleButton.textContent = "Purple";
+    purpleButton.addEventListener("mouseover", changeBGGreen);
+    document.body.appendChild(purpleButton)
+
+}
+clickMebutton.addEventListener("click", createAddButton)
+
+
+
+
+function createPara()
+{
+    const paragraph = document.createElement("p");
+    paragraph.textContent = "Hello There";
+    paragraph.addEventListener("mouseover", changeBGGreen);
+    document.body.appendChild(paragraph)
+
+}
+
+clickMebutton.addEventListener("click", createPara)
+
+
+function changeBGGreen(event) 
 {
 
-clickMebutton.textContent = "Clicked";
 
-if (clickMebutton.textContent === "Click Me!"){
-    clickMebutton.textContent = "Clicked"
-}
+    event.target.classList.add("green-background");
 
-if (clickMebutton.textContent === "Clicked"){
-
-    clickMebutton,textContent = "Click Me!"
 
 }
 
-}
-
-clickMebutton.addEventListener("Click", changeText)
+clickMebutton.addEventListener("click", changeBGGreen);
